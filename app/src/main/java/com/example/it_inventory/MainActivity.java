cb93b1abfd0feba6dffa0a0557933c463d8b0598
jonas <jonas.pilloud@students.hevs.ch>
2019-02-26 15:32:46 +0100

    test2

--- app/src/main/java/com/example/it_inventory/MainActivity.java
+++ app/src/main/java/com/example/it_inventory/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //test1
+    //test12
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
