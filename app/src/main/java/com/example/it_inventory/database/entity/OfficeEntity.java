96ae655b5d5196342d24765862800b88134ec096
jonas <jonas.pilloud@students.hevs.ch>
2019-03-12 13:10:39 +0100

    Database

--- app/src/main/java/com/example/it_inventory/database/entity/OfficeEntity.java
+++ app/src/main/java/com/example/it_inventory/database/entity/OfficeEntity.java
@@ -3,13 +3,12 @@ package com.example.it_inventory.database.entity;
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
-import android.support.annotation.NonNull;
 
-@Entity
+
+@Entity (tableName = "offices")
 public class OfficeEntity {
-    @PrimaryKey
-    @NonNull
-    private int id;
+    @PrimaryKey (autoGenerate = true)
+    private long id;
     @ColumnInfo(name = "Floor")
     private int floor;
     @ColumnInfo (name = "Building")
@@ -22,4 +21,56 @@ public class OfficeEntity {
     private String country;
     @ColumnInfo (name = "IdWorkstation")
     private int idWorkstation;
+
+    public long getId() {
+        return id;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(String building) {
+        this.building = building;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getIdWorkstation() {
+        return idWorkstation;
+    }
+
+    public void setIdWorkstation(int idWorkstation) {
+        this.idWorkstation = idWorkstation;
+    }
 }
--- app/src/main/java/com/example/it_inventory/database/entity/WorkstationEntity.java
+++ app/src/main/java/com/example/it_inventory/database/entity/WorkstationEntity.java
@@ -4,11 +4,10 @@ import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
 
-@Entity
+@Entity (tableName = "workstations")
 public class WorkstationEntity {
-    @PrimaryKey
-    @NonNull
-    private int id ;
+    @PrimaryKey (autoGenerate = true)
+    private long id ;
     @ColumnInfo(name = "Screens")
     private boolean screens;
     @ColumnInfo (name = "Portable")
@@ -24,4 +23,63 @@ public class WorkstationEntity {
     @ColumnInfo (name = "Keyboard_Type")
     private String keyboardType;
 
+    public long getId(){
+        return id ;
+    }
+
+    public boolean isScreens() {
+        return screens;
+    }
+
+    public void setScreens(boolean screens) {
+        this.screens = screens;
+    }
+
+    public boolean isPortable() {
+        return portable;
+    }
+
+    public void setPortable(boolean portable) {
+        this.portable = portable;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public int getStorage() {
+        return storage;
+    }
+
+    public void setStorage(int storage) {
+        this.storage = storage;
+    }
+
+    public String getProcessor() {
+        return processor;
+    }
+
+    public void setProcessor(String processor) {
+        this.processor = processor;
+    }
+
+    public String getKeyboardType() {
+        return keyboardType;
+    }
+
+    public void setKeyboardType(String keyboardType) {
+        this.keyboardType = keyboardType;
+    }
 }
