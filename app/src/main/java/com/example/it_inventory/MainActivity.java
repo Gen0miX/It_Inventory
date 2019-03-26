ccd5b1fe4ad8cf4bbb58fbd384a0e0380d0de3aa
dune211 <samuel.pintodasilva@students.hevs.ch>
2019-03-26 13:09:39 +0100

    Update: MainActivity (offices list)

--- app/src/main/java/com/example/it_inventory/MainActivity.java
+++ app/src/main/java/com/example/it_inventory/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.it_inventory;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,9 +12,16 @@ import android.widget.EditText;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.it_inventory.database.entity.OfficeEntity;
+import com.example.it_inventory.viewModel.viewModel.office.OfficeListViewModel;
+
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
+    private OfficeListViewModel officeListViewModel;
 
 
 
@@ -25,6 +36,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        officeListViewModel = ViewModelProviders.of(this).get(officeListViewModel.getClass());
+        officeListViewModel.getOffices().observe(this, new Observer<List<OfficeEntity>>() {
+            @Override
+            public void onChanged(@Nullable List<OfficeEntity> officeEntities) {
+                Toast.makeText(MainActivity.this, "On changed", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
