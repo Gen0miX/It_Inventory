451ca2dae41a9462a739e749b4948d361a7de372
dune211 <samuel.pintodasilva@students.hevs.ch>
2019-03-30 21:30:23 +0100

    Dark mode 2.0 --> functionnal

--- app/src/main/java/com/example/it_inventory/ui/office/OfficeActivity.java
+++ app/src/main/java/com/example/it_inventory/ui/office/OfficeActivity.java
@@ -45,13 +45,14 @@ public class OfficeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES)
         {
             setTheme(R.style.DarkTheme);
         }
         else
             setTheme(R.style.AppTheme);
-
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_office);
 
